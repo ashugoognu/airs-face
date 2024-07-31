7684d245f79b26d21613e64dcae691d0e7c7696b
anupamvishwakarma-goognu <anupam.kumar@goognu.com>
2024-07-31 16:07:49 +0530

    commit

--- src/Components/ResumeCards/ResumeCard.jsx
+++ src/Components/ResumeCards/ResumeCard.jsx
@@ -2,26 +2,37 @@ import React from "react";
 import email from "../../assets/email.png";
 import call from "../../assets/call.png";
 import next from "../../assets/next-icon.png";
+import { BASE_URL } from "../../config";
 
-export const ResumeCard = () => {
+export const ResumeCard = ({ item }) => {
+  const handleResume = (fileUrl) => {
+    console.log(fileUrl.filepath);
+    const newUrl = `${BASE_URL}` + "/" + fileUrl.filepath;
+    window.open(newUrl, "_blank");
+  };
+
   return (
     <div className="card">
       <div className="top-details">
         <div className="left">
-          <h3>Rakesh </h3>
+          <h3>{item.sender_name} </h3>
         </div>
         <div className="right">
           <div>
-            <span className="email">
-              <img src={email} alt="email" />
-              rekesh@hiringgo.com
-            </span>
-            <span className="email">
-              <img src={call} alt="call" />
-              9876543212
-            </span>
+            {item.sender_email && (
+              <span className="email">
+                <img src={email} alt="email" />
+                {item.sender_email}
+              </span>
+            )}
+            {item.mobile && (
+              <span className="email">
+                <img src={call} alt="call" />
+                {item.mobile}
+              </span>
+            )}
           </div>
-          <button className="btn">
+          <button className="btn" onClick={() => handleResume(item)}>
             View Resume
             <img src={next} alt="next" />
           </button>
@@ -33,37 +44,29 @@ export const ResumeCard = () => {
             <span>Skills</span>
           </div>
           <div className="skills">
-            <span>Linux</span>
-            <span>Devops</span>
-            <span>Ubuntu</span>
-            <span>Linux</span>
-            <span>Devops</span>
-            <span>Ubuntu</span>
-            <span>Linux</span>
-            <span>Devops</span>
-            <span>Ubuntu</span>
-            <span>Linux</span>
-            <span>Devops</span>
-            <span>Ubuntu</span>
-            <span>Linux</span>
-            <span>Devops</span>
-            <span>Ubuntu</span>
+            {item.skills.length > 0 ? (
+              item.skills.map((skill, i) => {
+                return <span key={i}> {skill} </span>;
+              })
+            ) : (
+              <span> null </span>
+            )}
           </div>
         </div>
-        <div className="skill">
+        {/* <div className="skill">
           <div className="left-title">
             <span>EXPERIENCE</span>
           </div>
           <div>
             <span>5 years</span>
           </div>
-        </div>
+        </div> */}
         <div className="skill">
           <div className="left-title">
             <span>MATCHING SCOPE</span>
           </div>
           <div>
-            <span>50%-100%</span>
+            <span>{item.match_score ? item.match_score.toFixed(2) : 0}%</span>
           </div>
         </div>
       </div>
--- src/Pages/Candidates.jsx
+++ src/Pages/Candidates.jsx
@@ -1,64 +1,71 @@
 import React, { useRef, useState } from "react";
 import axios from "axios";
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Form from 'react-bootstrap/Form';
-import upload from '../assets/uploadicon.png'
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import upload from "../assets/uploadicon.png";
 import search from "../assets/searchicon.png";
 import docs from "../assets/document.png";
-import Skeleton from 'react-loading-skeleton'
+import Skeleton from "react-loading-skeleton";
 import { ResumeCard } from "../Components/ResumeCards/ResumeCard";
+import { BASE_URL } from "../config";
 
 export const Candidates = () => {
   const [loader, setLoader] = useState(false);
-  const [count, setCount] = useState(4)
-  const [candiData, setCandiData] = useState([{}, {}, {}, {}, {}])
-  const [candiResume, setCandiResume] = useState([])
+  const [count, setCount] = useState(4);
+  const [candiData, setCandiData] = useState([]);
   const lableRef = useRef();
 
   const [jdValue, setJdValue] = useState({
-    jd: ''
+    jd: "",
   });
 
   const handleJdCandidates = async () => {
-    setLoader(true)
+    setLoader(true);
     var options = {
-      method: 'POST',
-      url: `${process.env.REACT_APP_URL}/api/v1/filter-resumes/filter-resumes-from-jd-skillss`,
+      method: "POST",
+      url: `${BASE_URL}/api/v1/filter-resumes/filter-resumes-from-jd-skillss`,
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      data: jdValue
+      data: jdValue,
     };
 
-    axios.request(options).then(function (response) {
-      setCandiData(response.data)
-      setLoader(false)
-      setCandiResume(response.data.resumes_with_match_score)
-    }).catch(function (error) {
-      console.error(error);
-    });
-  }
+    axios
+      .request(options)
+      .then(function (response) {
+        setCandiData(response.data);
+        setLoader(false);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 
   const handleFile = (e) => {
-    const file = e.target.files[0]
+    const file = e.target.files[0];
     if (file) {
-      lableRef.current.innerText = file.name
+      lableRef.current.innerText = file.name;
     }
-  }
+  };
 
   return (
     <>
       <div className="m-5 mt-0 candidate-resume">
         <div className="top d-flex candidate-header">
-          <h2 > Find Best Resumes </h2>
+          <h2> Find Best Resumes </h2>
         </div>
         <div className="jd-text-fields d-flex align-items-center w-100 gap-3 mb-3">
-          <FloatingLabel controlId="floatingTextarea2" label="Enter Job description...">
+          <FloatingLabel
+            controlId="floatingTextarea2"
+            label="Enter Job description..."
+          >
             <Form.Control
               as="textarea"
               placeholder="Leave a comment here"
               value={jdValue.jd}
-              onChange={(e) => setJdValue(prev => ({ ...prev, jd: e.target.value }))}
+              onChange={(e) =>
+                setJdValue((prev) => ({ ...prev, jd: e.target.value }))
+              }
             />
           </FloatingLabel>
           <span> OR </span>
@@ -73,46 +80,53 @@ export const Candidates = () => {
         </div>
       </div>
       <div className="filtered-resume">
-        {
-          loader ?
-            <div className="card d-flex gap-2">
-              <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-                <div style={{ flex: '0 0 50%' }}>
-                  <Skeleton width="100%" />
-                </div>
-                <div style={{ flex: '0 0 15%' }}>
-                  <Skeleton width="100%" />
-                </div>
+        {loader ? (
+          <div className="card d-flex gap-2">
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "100%",
+              }}
+            >
+              <div style={{ flex: "0 0 50%" }}>
+                <Skeleton width="100%" />
               </div>
-              <Skeleton height={50} />
-              <Skeleton count={2} />
+              <div style={{ flex: "0 0 15%" }}>
+                <Skeleton width="100%" />
+              </div>
             </div>
-            :
-            candiData.length > 0 ?
-              candiData.map((item, i) => {
-                if (i < count) {
-                  return (
-                    <ResumeCard />
-                  )
-                }
-              })
-              :
-              <div className="card">
-                <div className="empty-string">
-                  <img src={docs} alt="Empty Cart" />
-                  <h4>Your Search is Empty </h4>
-                  <p> Enter Job Description or upload file into above. </p>
-                </div>
-              </div>
-        }
-        {
-          candiData.length > 4 &&
-          <div className={`${candiData.length === count ? 'd-none' : ''} view-more`}>
-            <button className="btn" onClick={() => setCount(candiData.length)}> Load more results... </button>
+            <Skeleton height={50} />
+            <Skeleton count={2} />
           </div>
-        }
-
-      </div >
+        ) : candiData.length > 0 ? (
+          candiData.map((item, i) => {
+            if (i < count) {
+              return <ResumeCard item={item} key={i} />;
+            }
+          })
+        ) : (
+          <div className="card">
+            <div className="empty-string">
+              <img src={docs} alt="Empty Cart" />
+              <h4>Your Search is Empty </h4>
+              <p> Enter Job Description or upload file into above. </p>
+            </div>
+          </div>
+        )}
+        {candiData.length > 4 && (
+          <div
+            className={`${
+              candiData.length === count ? "d-none" : ""
+            } view-more`}
+          >
+            <button className="btn" onClick={() => setCount(candiData.length)}>
+              {" "}
+              Load more results...{" "}
+            </button>
+          </div>
+        )}
+      </div>
     </>
   );
 };
